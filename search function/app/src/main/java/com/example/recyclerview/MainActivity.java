71c87efbcd6527b8a00d632f0a7905fff6710150
Dimuthu-abethunge <56988418+Dimuthu-abethunge@users.noreply.github.com>
2020-11-28 00:01:52 +0530

    Add files via upload

---
+++ search function/app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerAdapter recyclerAdapter;
+
+    List<String> moviesList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        moviesList = new ArrayList<>();
+        moviesList.add("Iron Man");
+        moviesList.add("The Incredible Hulk");
+        moviesList.add("Iron Man 2");
+        moviesList.add("Thor");
+        moviesList.add("Captain America: The First Avenger");
+        moviesList.add("The Avengers");
+        moviesList.add("Iron Man 3");
+        moviesList.add("Thor: The Dark World");
+        moviesList.add("Captain America: The Winter Soldier");
+        moviesList.add("Guardians of the Galaxy");
+        moviesList.add("Avengers: Age of Ultron");
+        moviesList.add("Ant-Man");
+        moviesList.add("Captain America: Civil War");
+        moviesList.add("Doctor Strange");
+        moviesList.add("Guardians of the Galaxy Vol. 2");
+        moviesList.add("Spider-Man: Homecoming");
+        moviesList.add("Thor: Ragnarok");
+        moviesList.add("Black Panther");
+        moviesList.add("Avengers: Infinity War");
+        moviesList.add("Ant-Man and the Wasp");
+        moviesList.add("Captain Marvel");
+        moviesList.add("Avengers: Endgame");
+        moviesList.add("Spider-Man: Far From Home");
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerAdapter = new RecyclerAdapter(moviesList);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(recyclerAdapter);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(dividerItemDecoration);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                recyclerAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
